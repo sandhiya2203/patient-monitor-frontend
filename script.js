748b290d2c057a1dfd6d735d9e775aa87e816227
sandhiya2203 <107338809+sandhiya2203@users.noreply.github.com>
2026-07-13 10:54:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+async function loadPatientData() {
+
+    try {
+
+        const response = await fetch("/patient");
+
+        if (!response.ok) {
+            throw new Error("Unable to fetch patient data");
+        }
+
+        const data = await response.json();
+
+        // Update Status
+        document.getElementById("status").textContent = data.status;
+
+        // Update Heart Rate
+        document.getElementById("heart").textContent =
+            data.heart_rate ?? 0;
+
+        // Update Blood Pressure
+        document.getElementById("bp").innerHTML =
+            `${data.systolic_bp}`;
+
+        // Update SpO2
+        document.getElementById("spo2").textContent =
+            data.spo2 ?? 0;
+
+        // Change status color
+        const status = document.getElementById("status");
+
+        if (data.status === "Connected") {
+
+            status.style.color = "green";
+
+        }
+        else if (data.status === "Searching Provider...") {
+
+            status.style.color = "orange";
+
+        }
+        else if (data.status === "Provider Not Found") {
+
+            status.style.color = "red";
+
+        }
+        else {
+
+            status.style.color = "#1565c0";
+
+        }
+
+    }
+    catch (error) {
+
+        console.log(error);
+
+        document.getElementById("status").textContent = "Connection Error";
+        document.getElementById("status").style.color = "red";
+
+    }
+
+}
+
+
+// Load immediately
+loadPatientData();
+
+// Refresh every second
+setInterval(loadPatientData, 1000);
